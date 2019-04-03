bfa13d18629c72d176909054d363609e0582c3b5
Alvin N <alvinnjenga@gmail.com>
2019-04-03 11:16:08 +0100

    Completed front end!

--- server/index.js
+++ server/index.js
@@ -11,6 +11,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    next();
+});
 
 app.use('/api/cities', require('./api/cities'));
 app.use('/api/weather', require('./api/weather'));
